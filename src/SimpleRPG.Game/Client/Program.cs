6795a73ca917a603749224518d5c27263162f004
Joshua Thompson <ysovuka@gmail.com>
2022-02-27 11:57:11 -0600

    Added Blazorise and setup project to use it.

--- src/SimpleRPG.Game/Client/Program.cs
+++ src/SimpleRPG.Game/Client/Program.cs
@@ -1,12 +1,24 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using SimpleRPG.Game.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazorise(o =>
+{
+    o.ChangeTextOnKeyPress = false;
+})
+.AddBootstrapProviders();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
 
 await builder.Build().RunAsync();
