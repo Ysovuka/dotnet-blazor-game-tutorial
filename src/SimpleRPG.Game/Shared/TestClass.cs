c23e0a04facb65e7f29f843f8bbf2e5cbe5ad3b9
Joshua Thompson <ysovuka@gmail.com>
2022-03-07 12:55:49 -0600

    Add unit tests

---
+++ src/SimpleRPG.Game/Shared/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRPG.Game
+{
+    public class TestClass
+    {
+        public string Name { get; private set; } = string.Empty;
+
+        public bool DoSomething(string value)
+        {
+            Name = value;
+            return true;
+        }
+    }
+}
