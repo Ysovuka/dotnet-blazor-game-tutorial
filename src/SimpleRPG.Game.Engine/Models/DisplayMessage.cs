c609e669b113c75d37c801a4ff133ddbed6ef0dd
Joshua Thompson <ysovuka@gmail.com>
2022-03-07 20:56:31 -0600

    Add game messages

---
+++ src/SimpleRPG.Game.Engine/Models/DisplayMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleRPG.Game.Engine.Models;
+
+public class DisplayMessage
+{
+    public DisplayMessage(string title, IList<string> messages)
+    {
+        Title = title;
+        Messages = messages;
+    }
+
+    public string Title { get; } = string.Empty;
+
+    public IList<string> Messages { get; }
+}
