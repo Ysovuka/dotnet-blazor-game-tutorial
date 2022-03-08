5a6352656f0f9f99de0ef026e5b346c74df833f7
Joshua Thompson <ysovuka@gmail.com>
2022-03-07 20:21:36 -0600

    Add item factory and basic weapons

---
+++ src/SimpleRPG.Game.Engine/Factories/ItemFactory.cs
@@ -0,0 +1,18 @@
+﻿using SimpleRPG.Game.Engine.Models;
+
+namespace SimpleRPG.Game.Engine.Factories;
+
+internal static class ItemFactory
+{
+    private static List<GameItem> _standardGameItems = new List<GameItem>
+    {
+        new Weapon(1001, "Pointy Stick", 1, 1, 2),
+        new Weapon(1002, "Rusty Sword", 5, 1, 3)
+    };
+
+    public static GameItem? CreateGameItem(int itemTypeID)
+    {
+        var standardItem = _standardGameItems.FirstOrDefault(i => i.ItemTypeID == itemTypeID);
+        return standardItem?.Clone();
+    }
+}
---
+++ src/SimpleRPG.Game.Engine/Models/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRPG.Game.Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage)
+            : base(itemTypeID, name, price)
+        {
+            MinimumDamage = minDamage;
+            MaximumDamage = maxDamage;
+        }
+
+        public int MinimumDamage { get; set; }
+
+        public int MaximumDamage { get; set; }
+
+        public override GameItem Clone() =>
+            new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage);
+    }
+}
